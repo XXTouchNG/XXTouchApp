726407d20de5c5df6ad96e816e6eee41c5b2151a
i_82 <i.82@qq.com>
2016-10-30 23:30:57 +0800

    XX File Viewer

---
+++ XXTouchApp/Global/Catalogs/NSData+plistData.h
@@ -0,0 +1,14 @@
+//
+//  NSData+plistData.h
+//  XXTouchApp
+//
+//  Created by Zheng on 30/10/2016.
+//  Copyright © 2016 Zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (plistData)
+- (NSString *)plistString;
+
+@end
---
+++ XXTouchApp/Global/Catalogs/NSData+plistData.m
@@ -0,0 +1,32 @@
+//
+//  NSData+plistData.m
+//  XXTouchApp
+//
+//  Created by Zheng on 30/10/2016.
+//  Copyright © 2016 Zheng. All rights reserved.
+//
+
+#import "NSData+plistData.h"
+
+@implementation NSData (plistData)
+
+- (NSString *)plistString {
+    id plistData = [NSDictionary dictionaryWithPlistData:self];
+    if (!plistData)
+    {
+        plistData = [NSArray arrayWithPlistData:self];
+    }
+    if (!plistData)
+    {
+        return nil;
+    }
+    if (
+        [plistData isKindOfClass:[NSDictionary class]] ||
+        [plistData isKindOfClass:[NSArray class]]
+        ) {
+        return [plistData plistString];
+    }
+    return nil;
+}
+
+@end
